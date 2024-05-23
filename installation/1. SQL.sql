e340f1299d2643a5d6843164cb643dd6cbb8957a
OlegShlihtemayer <oshlihtemayer@onevizion.com>
2024-05-23 16:09:18 +0700

    Rules Package 2

--- installation/1. SQL.sql
+++
@@ -1,9 +0,0 @@
-declare
-v_cnt int;
-begin 
-v_cnt := util_module.get_module_id ('Test_int');
-INSERT INTO message (process, message)
-VALUES (111,v_cnt);
-util_module.enable_module_rules(:p_module_id);
-end;
-
